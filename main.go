fbc75d2a2c1bc3acd4187052636ac65857d22e4e
syaiamira <syahidatulamira06@gmail.com>
2023-03-28 12:20:15 +0800

    3rd

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	swagger "github.com/arsmn/fiber-swagger/v2"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
+
+	app.Get("/swagger/*", swagger.HandlerDefault) // default
+
+	app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
+		URL:         "http://example.com/doc.json",
+		DeepLinking: false,
+		// Expand ("list") or Collapse ("none") tag groups by default
+		DocExpansion: "none",
+		// Prefill OAuth ClientId on Authorize popup
+		OAuth: &swagger.OAuthConfig{
+			AppName:  "OAuth Provider",
+			ClientId: "21bb4edc-05a7-4afc-86f1-2e151e4ba6e2",
+		},
+		// Ability to change OAuth2 redirect uri location
+		OAuth2RedirectUrl: "http://localhost:8080/swagger/oauth2-redirect.html",
+	}))
 
 	app.Listen(":8080")
 }
